38b4cbc82b6a1ab95cab396fa2ac3fdbb33e393f
Neville Yee <neville.yee@alumni.york.ac.uk>
2022-01-12 17:59:15 +0000

    Added run script for program.

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-python3 pdb-plane-calc.py h11-h4-rbd-simp.pdb 490 52 \
+python3 pdb-plane-calc.py -e h11-h4-rbd-simp.pdb 490 52 \
 	--chainA=E \
 	--chainB=F \
 	--nameA=cg,cd1,ce1,cz,ce2,cd2 \
-	--nameB=nh2,ne,nh1
+	--nameB=nh2,ne,nh1 \
+	-np=1000000
